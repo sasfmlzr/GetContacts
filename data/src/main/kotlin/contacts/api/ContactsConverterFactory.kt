52149d438c9e1cf273d8cecb56690e28d5c0431f
sasfmlzr <sasfmlzr@yandex.ru>
2019-02-22 15:48:54 +0300

    Refactoring api module(di). Divide retrofit and gson factory.

---
+++ data/src/main/kotlin/contacts/api/ContactsConverterFactory.kt
@@ -0,0 +1,44 @@
+package contacts.api
+
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonObject
+import contact.api.location.GetLocation
+import contact.api.model.contact.OwnerContacts
+import org.joda.time.LocalDateTime
+import org.joda.time.format.DateTimeFormat
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ContactsConverterFactory {
+
+    companion object {
+        fun getOwnerContactConverterFactory(): GsonConverterFactory {
+            val ownerContactsParser = GsonBuilder()
+                    .registerTypeAdapter(OwnerContacts::class.java,
+                            JsonDeserializer<OwnerContacts> { json, _, _ ->
+                                val jComment = json.asJsonObject
+                                val owner = getLocationParcer().fromJson(jComment,
+                                        OwnerContacts::class.java)
+                                owner
+                            })
+                    .create()
+            return GsonConverterFactory.create(ownerContactsParser)
+        }
+
+        private fun getLocationParcer() = GsonBuilder()
+                .registerTypeAdapter(
+                        GetLocation::class.java,
+                        JsonDeserializer<GetLocation> { locationJson, _, _ ->
+                            val loc = locationJson as JsonObject
+                            val date = loc.get("date").asString
+                            val pattern = DateTimeFormat
+                                    .forPattern("YYYY-MM-dd HH:mm")
+                            GetLocation(loc.get("latitude")
+                                    .asDouble,
+                                    loc.get("longitude").asDouble,
+                                    LocalDateTime
+                                            .parse(date, pattern))
+                        })
+                .create()
+    }
+}
--- data/src/main/kotlin/contacts/di/ApiModule.kt
+++ data/src/main/kotlin/contacts/di/ApiModule.kt
@@ -6,16 +6,14 @@ import com.google.gson.JsonObject
 import contact.api.location.GetLocation
 import contact.api.model.contact.OwnerContacts
 import contacts.api.ContactsApi
+import contacts.api.ContactsConverterFactory
 import dagger.Module
 import dagger.Provides
 import okhttp3.HttpUrl
 import org.joda.time.format.DateTimeFormat
-import org.joda.time.format.DateTimeFormatter
-import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.*
 import javax.inject.Singleton
 
 
@@ -35,39 +33,11 @@ internal class ApiModule {
                 .scheme("http")
                 .build()
 
-        val gson = GsonBuilder()
-                .registerTypeAdapter(OwnerContacts::class.java,
-                        JsonDeserializer<OwnerContacts> { json, _, _ ->
-                            val jComment = json.asJsonObject
-                            val gsonParser = GsonBuilder()
-                                    .registerTypeAdapter(
-                                            GetLocation::class.java,
-                                            JsonDeserializer<GetLocation> { locationJson, _, _ ->
-                                                val loc = locationJson as JsonObject
-                                                val date = loc.get("date").asString
-                                                val pattern = DateTimeFormat
-                                                        .forPattern("YYYY-MM-dd HH:mm")
-                                                GetLocation(loc.get("latitude")
-                                                        .asDouble,
-                                                        loc.get("longitude").asDouble,
-                                                        org.joda.time.LocalDateTime
-                                                                .parse(date, pattern))
-                                            })
-                                    .create()
-                            val owner = gsonParser.fromJson(jComment, OwnerContacts::class.java)
-                            owner
-                        })
-                .create()
-
-        val gsonConverterFactory = GsonConverterFactory.create(gson)
-
         val retrofit = Retrofit.Builder()
                 .baseUrl(httpUrl)
-                .addConverterFactory(gsonConverterFactory)
+                .addConverterFactory(ContactsConverterFactory.getOwnerContactConverterFactory())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
         return retrofit.create(ContactsApi::class.java)
     }
-
-
 }
