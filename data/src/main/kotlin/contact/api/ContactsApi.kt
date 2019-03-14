139616c0cca77f5db9bc799fd4bc43056d1d3c70
sasfmlzr <sasfmlzr@yandex.ru>
2019-03-14 12:46:18 +0300

    Delete unused retrofit requests.

--- data/src/main/kotlin/contact/api/ContactsApi.kt
+++ data/src/main/kotlin/contact/api/ContactsApi.kt
@@ -1,19 +1,10 @@
 package contact.api
 
-import contact.api.location.GetLocation
 import contact.api.model.contact.OwnerContacts
 import io.reactivex.Single
-import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Path
 
 interface ContactsApi {
     @GET("contacts/get")
     fun getAllOwnerContacts(): Single<List<OwnerContacts>>
-
-    @GET("contacts/get")
-    fun getOwnerContacts(): Call<List<OwnerContacts>>
-
-    @GET("contacts/getlocation/{id}")
-    fun getOwnerById(@Path("id") nameOwner: String): Call<GetLocation>
 }
